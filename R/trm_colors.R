c330b1f491ee0ef094432a9d6868408d2e7b8107
trm-hsha <hsha@trm.dk>
2026-07-10 14:18:13 +0200

    Farver opdateret

--- R/trm_colors.R
+++ R/trm_colors.R
@@ -1,21 +1,47 @@
+#' #' Farver i TRM
+#' #'
+#' #' @param farver Choose between these colors c("blaa", "orange","graa", "gul","groen", "lyseblaa")
+#' #' @export
+#' trm_colors <- function(farver){
+#' 
+#'   farver_ind <- tolower(farver)
+#' 
+#'   farver_valg <- c("blaa" = grDevices::rgb(0,169,224, max=255), "orange" = grDevices::rgb(249,186,4, max=255),
+#'     "graa" = grDevices::rgb(173,175,175, max=255), "gul" = grDevices::rgb(223,223,0, max=255),
+#'     "groen" = grDevices::rgb(128,172,158, max=255), "lyseblaa"= grDevices::rgb(91,198,232, max=255))
+#' 
+#'   til_out <- farver_valg[order(factor(names(farver_valg), levels = farver_ind))] |>
+#'     (\(y) y[names(y) %in% farver_ind])()
+#' 
+#'   return(as.character(til_out))
+#' }
+
+
+
 #' Farver i TRM
 #'
-#' @param farver Choose between these colors c("blaa", "orange","graa", "gul","groen", "lyseblaa")
+#' @param farver Vælg mellem c("blaa", "cyan", "orange", "groen", "roed")
+#' @param lys Lyshedsgrad: 0 (fuld), 1, 2 eller 3
 #' @export
-trm_colors <- function(farver){
-
+trm_colors <- function(farver, lys = 0) {
+  
   farver_ind <- tolower(farver)
-
-  farver_valg <- c("blaa" = grDevices::rgb(0,169,224, max=255), "orange" = grDevices::rgb(249,186,4, max=255),
-    "graa" = grDevices::rgb(173,175,175, max=255), "gul" = grDevices::rgb(223,223,0, max=255),
-    "groen" = grDevices::rgb(128,172,158, max=255), "lyseblaa"= grDevices::rgb(91,198,232, max=255))
-
-  til_out <- farver_valg[order(factor(names(farver_valg), levels = farver_ind))] |>
-    (\(y) y[names(y) %in% farver_ind])()
-
-  return(as.character(til_out))
+  
+  palette <- list(
+    blaa   = c("#002447", "#536D86", "#A6B6C5", "#CFDAE5"),
+    cyan   = c("#127EA3", "#469CB9", "#7BB9CE", "#AFD7E4"),
+    orange = c("#F2683E", "#F58A6A", "#F8AB95", "#FBCDC1"),
+    groen  = c("#086B39", "#43916C", "#7EB89E", "#B9DED1"),
+    roed   = c("#B50012", "#C9414D", "#DE8287", "#F2C3C2")
+  )
+  
+  ukendt <- setdiff(farver_ind, names(palette))
+  if (length(ukendt) > 0) {
+    stop("Ukendt farve: ", paste(ukendt, collapse = ", "),
+         ". Vaelg mellem: ", paste(names(palette), collapse = ", "))
+  }
+  if (!all(lys %in% 0:3)) stop("lys skal vaere 0, 1, 2 eller 3")
+  
+  lys <- rep_len(lys, length(farver_ind))
+  unname(mapply(function(f, l) palette[[f]][l + 1], farver_ind, lys))
 }
-
-
-
-
